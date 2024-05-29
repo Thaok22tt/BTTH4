c5a428d3eb2451c43fb102c87600fb4fd3338b33
Thaok22tt <167071147+Thaok22tt@users.noreply.github.com>
2024-05-29 22:07:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 const triangle = document.getElementById('triangle');
 const details = document.getElementById('details');
-const myText = document.getElementById('myText');
-const detailsText = document.getElementById('details-text');
+
+let isDetailsShown = false; // Flag to track visibility state
 
 triangle.addEventListener('click', () => {
-  details.style.display = 'block';
-  const icon = triangle.querySelector('.icon');
-  const text = triangle.querySelector('span');
-
-  if (text.textContent === 'Show-details') {
-    text.textContent = 'Hide-details';
-    icon.classList.remove('bxs-right-arrow');
-    icon.classList.add('bxs-down-arrow');
-  } else {
+  if (isDetailsShown) {
+    details.style.display = 'none';
+    const icon = triangle.querySelector('.icon');
+    const text = triangle.querySelector('span');
     text.textContent = 'Show-details';
     icon.classList.remove('bxs-down-arrow');
     icon.classList.add('bxs-right-arrow');
+    isDetailsShown = false;
+  } else {
+    details.style.display = 'block';
+    const icon = triangle.querySelector('.icon');
+    const text = triangle.querySelector('span');
+    text.textContent = 'Hide-details';
+    icon.classList.remove('bxs-right-arrow');
+    icon.classList.add('bxs-down-arrow');
+    isDetailsShown = true;
   }
 });
+
 
 
 
